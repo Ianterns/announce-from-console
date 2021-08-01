11fbdf43d13073fdf982e6855ff91332b17c7763
lndu <84151766+lndu@users.noreply.github.com>
2021-08-01 18:51:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require('discord.js'_
+
+let y = process.openStdin();
+y.addListener("data", (res) => {
+  let x = res.toString().trim().split(/ +/g); 
+  var guilds = client.guilds.cache
+  try {
+    const embed = new MessageEmbed()
+    .setDescription(`**Announcment**
+    \n
+    \`${x.join(' ')}\``)
+    guilds.forEach(guild =>{
+        
+        guild.channels.cache.find(c => c.type === 'text').send(embed)
+
+    });
+  } catch (err) {
+      console.log(err)
+  }
+  
+});
